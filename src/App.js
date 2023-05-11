677652749d1bf3ebc1482755064a0a4f2173077c
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-11 16:23:53 +0300

    feat: added tailwindcss and created the necessary components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import NoteList from './components/NoteList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className='container'>
+      <SearchBar />
+      <NoteList />
+    </div>
   );
 }
 
